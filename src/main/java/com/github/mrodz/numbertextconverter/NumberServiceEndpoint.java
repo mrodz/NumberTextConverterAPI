88427505a6f6ca592177d73a2550120b197cb0f2
mrodz <79176075+mrodz@users.noreply.github.com>
2022-01-21 23:13:20 -0800

    Functional Build

--- src/main/java/com/github/mrodz/numbertextconverter/NumberServiceEndpoint.java
+++ src/main/java/com/github/mrodz/numbertextconverter/NumberServiceEndpoint.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.Array;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 import java.util.function.Function;
 
 @RestController
@@ -17,8 +17,7 @@ public class NumberServiceEndpoint {
 
         public ConvertedNumber(BigInteger number) {
             this.number = number;
-            //            System.out.println("\t\t$\t" + temp);
-            this.result = getStringRepresentation(number);
+            this.result = getStringRepresentationOfNumber(number);
         }
 
         HashMap<Integer, String> decimalNumberToStringMapping = new HashMap<>();
@@ -54,52 +53,165 @@ public class NumberServiceEndpoint {
             decimalNumberToStringMapping.put(90, "ninety");
         }
 
-        HashMap<BigInteger, String> powersToStringMapping = new HashMap<>();
+        TreeMap<BigInteger, String> powersToStringMapping = new TreeMap<>();
 
         {
             powersToStringMapping.put(BigInteger.ONE, "");
-            powersToStringMapping.put(BigInteger.TEN, "teen");
-            powersToStringMapping.put(BigInteger.valueOf(100), "hundred");
-            powersToStringMapping.put(BigInteger.valueOf(1_000), "thousand");
-//            powersToStringMapping.put(BigInteger.valueOf(100_000), "hundred-thousand");
-            powersToStringMapping.put(BigInteger.valueOf(1_000_000), "million");
-//            powersToStringMapping.put(BigInteger.valueOf(100_000_000), "hundred-million");
-            powersToStringMapping.put(BigInteger.valueOf(1_000_000_000), "billion");
-//            powersToStringMapping.put(new BigInteger("100000000000"), "hundred-billion");
-            powersToStringMapping.put(new BigInteger("1000000000000"), "trillion");
-
+            powersToStringMapping.put(BigInteger.TEN, "");
+            powersToStringMapping.put(BigInteger.TEN.pow(2), "hundred");
+            powersToStringMapping.put(BigInteger.TEN.pow(3), "thousand");
+            powersToStringMapping.put(BigInteger.TEN.pow(6), "million");
+            powersToStringMapping.put(BigInteger.TEN.pow(9), "billion");
+            powersToStringMapping.put(BigInteger.TEN.pow(12), "trillion");
+            powersToStringMapping.put(BigInteger.TEN.pow(15), "quadrillion");
+            powersToStringMapping.put(BigInteger.TEN.pow(18), "quintillion");
+            powersToStringMapping.put(BigInteger.TEN.pow(21), "sextillion");
+            powersToStringMapping.put(BigInteger.TEN.pow(24), "septillion");
+            powersToStringMapping.put(BigInteger.TEN.pow(27), "octillion");
+            powersToStringMapping.put(BigInteger.TEN.pow(30), "nonillion");
+            powersToStringMapping.put(BigInteger.TEN.pow(33), "decillion");
+            powersToStringMapping.put(BigInteger.TEN.pow(36), "undecillion");
+            powersToStringMapping.put(BigInteger.TEN.pow(39), "duodecillion");
+            powersToStringMapping.put(BigInteger.TEN.pow(42), "tredecillion");
+            powersToStringMapping.put(BigInteger.TEN.pow(45), "quattuordecillion");
+            powersToStringMapping.put(BigInteger.TEN.pow(48), "quindecillion");
+            powersToStringMapping.put(BigInteger.TEN.pow(51), "sexdecillion");
+            powersToStringMapping.put(BigInteger.TEN.pow(54), "septendecillion");
+            powersToStringMapping.put(BigInteger.TEN.pow(57), "octodecillion");
+            powersToStringMapping.put(BigInteger.TEN.pow(60), "novemdecillion");
+            powersToStringMapping.put(BigInteger.TEN.pow(63), "vigintillion");
+            powersToStringMapping.put(BigInteger.TEN.pow(100), "googol");
+            powersToStringMapping.put(BigInteger.TEN.pow(303), "centillion");
         }
 
-        private String getStringRepresentation(BigInteger number) {
+        private String getStringRepresentationOfSmallNumber(BigInteger number) {
             int steps = number.toString().length() - 1;
-            return getStringRepresentation(new ArrayList<>(), number, steps, steps);
+            return this.getStringRepresentationOfSmallNumber(new ArrayList<>(), number, steps);
         }
 
-        private String getStringRepresentation(ArrayList<String> result, BigInteger number, int steps, final int totalSteps) {
-            System.out.println("result = " + result + ", number = " + number + ", steps = " + steps);
-            BigInteger power = BigInteger.TEN.pow(steps /*- (steps % 3)*/);
-            System.out.println("power = " + power);
-            BigInteger reduced = number./*mod*/divide(power);
-            System.out.println("reduced = " + reduced);
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        private String getStringRepresentationOfNumber(BigInteger number) {
+            if (number.compareTo(BigInteger.ZERO) == 0) return "zero";
 
-            if (reduced.compareTo(BigInteger.ZERO) >= 0 && steps > 0) {
+            String negativeAddon = number.compareTo(BigInteger.ZERO) < 0 ? "negative " : "";
 
-                Function<Object, String> errorString = (obj) -> String.format("\"[ERROR] %s\"", obj);
+            number = number.abs();
 
-                String decimalString = decimalNumberToStringMapping.getOrDefault(reduced.intValue(), errorString.apply(reduced.intValue()));
-                String powerString = powersToStringMapping.getOrDefault(power, errorString.apply(power));
+            if (number.toString().length() < 3) return negativeAddon.concat(getStringRepresentationOfSmallNumber(number));
 
-//                if (reduced.intValue() != 0) {
-                    result.add(decimalString);
-                    result.add(powerString);
-//                }
+            ArrayList<StringBuilder> segments = new ArrayList<>();
+            StringBuilder actual = new StringBuilder(number.toString());
+            StringBuilder buffer = new StringBuilder();
 
-                return getStringRepresentation(result, number, steps - 1, totalSteps);
-            } else {
+            actual.reverse();
+            for (int i = 1; i <= actual.length(); i++) {
+                buffer.append(actual.charAt(i - 1));
+                if (i % 3 == 0 || i == actual.length()) {
+                    segments.add(new StringBuilder(buffer));
+                    buffer.delete(0, buffer.length());
+                }
+            }
+
+            segments.forEach(StringBuilder::reverse);
+
+            segments = new ArrayList(Arrays.asList(reverseArray(segments.toArray())));
+
+            System.out.println("segments = " + segments);
+
+            ArrayList<String> result = new ArrayList<>();
+
+            Map.Entry[] arrayOfPowers = powersToStringMapping.entrySet().toArray(new Map.Entry[0]);
+
+            for (int i = segments.size() + 1, c = 0; ; i--, c++) {
+                if (!(c < segments.size() && i >= 0)) break; // make loop declaration smaller
+
+                String forThisGrouping = getStringRepresentationOfSmallNumber(new BigInteger(segments.get(c).toString()));
+
+                System.out.println("forThisGrouping = " + forThisGrouping);
+                System.out.println("arrayOfPowers = " + arrayOfPowers[i]);
+
+                if (!forThisGrouping.isEmpty()) {
+                    result.add(String.format("%s%s", forThisGrouping, arrayOfPowers[i].getValue()
+                            .equals(powersToStringMapping.get(BigInteger.valueOf(100)))
+                            ? "" : " " + arrayOfPowers[i].getValue()));
+                }
+            }
+
+            return negativeAddon.concat(result.stream().reduce("", (first, second) -> first.concat(" ").concat(second)).trim());
+        }
+
+        private static <T> T[] reverseArray(T[] arr) {
+            @SuppressWarnings("unchecked")
+            T[] newArr = (T[]) Array.newInstance(arr.getClass().getComponentType(), arr.length);
+            for (int i = 0, length = arr.length; i < length; i++) {
+                newArr[i] = arr[length - 1 - i];
+            }
+            return newArr;
+        }
+
+        private String getStringRepresentationOfSmallNumber(ArrayList<String> result, BigInteger number, int steps) {
+            if (steps < 0) {
                 StringBuilder fin = new StringBuilder();
                 result.forEach(s -> fin.append(s).append(' '));
-                return fin.substring(0, fin.length() - 1);
+                return handleEdgeCases(fin.substring(0, fin.length() - 1));
             }
+
+            BigInteger power = BigInteger.TEN.pow(steps/*- (steps % 3)*/);
+            int reduced = getNumberAtDecimalPlace(number, power);
+
+            Function<Object, String> errorString = (obj) -> String.format("\"[ERROR] %s\"", obj);
+
+            String decimalString = decimalNumberToStringMapping.getOrDefault(reduced, errorString.apply(reduced));
+            String powerString = powersToStringMapping.getOrDefault(power, errorString.apply(power));
+
+            int key = power.multiply(BigInteger.valueOf(reduced)).intValue();
+            if (decimalNumberToStringMapping.containsKey(key)) {
+                result.add(decimalNumberToStringMapping.get(key));
+            }
+            if (powerString.length() > 0 && reduced != 0) {
+                result.add(decimalString);
+                result.add(powerString);
+            }
+
+            return this.getStringRepresentationOfSmallNumber(result, number, steps - 1);
+        }
+
+        public String handleEdgeCases(String number) {
+            number = number.replaceAll("(\\s*zero)++", "").replaceAll("^\\s++|\\s++$", "");
+
+            number = number.replaceAll("(ten one)", "eleven");
+            number = number.replaceAll("(ten two)", "twelve");
+            number = number.replaceAll("(ten three)", "thirteen");
+            number = number.replaceAll("(ten four)", "fourteen");
+            number = number.replaceAll("(ten five)", "fifteen");
+            number = number.replaceAll("(ten six)", "sixteen");
+            number = number.replaceAll("(ten seven)", "seventeen");
+            number = number.replaceAll("(ten eight)", "eighteen");
+            number = number.replaceAll("(ten nine)", "nineteen");
+
+            return number;
+        }
+
+        /**
+         * Get an integer value at a number's specific power.
+         * <pre>
+         *     (1350, 100) -> 3
+         *     (543210, 10000) -> 4
+         * </pre>
+         * Follows this equation: <tt>((x-((x-(x%(t*10))))-(x%t))/t)</tt> where
+         * <tt>x</tt> is any number and <tt>t</tt> is the target decimal place.
+         *
+         * @param number a number
+         * @param target a multiple of 10. (1, 10, 100, 1000, 10000, etc.)
+         * @return the digit at a specified number's place value.
+         * @see #getStringRepresentationOfSmallNumber(BigInteger)
+         */
+        public static int getNumberAtDecimalPlace(final BigInteger number, BigInteger target) {
+            if (String.valueOf(target).length() > String.valueOf(number).length()) {
+                throw new IllegalArgumentException("Place value is too large");
+            }
+            return ((number.subtract((number.subtract(number.mod(target.multiply(BigInteger.TEN)))))
+                    .subtract(number.mod(target))).divide(target)).intValue();
         }
 
         public BigInteger getNumber() {
--- src/test/java/com/github/mrodz/numbertextconverter/NumberServiceEndpointTest.java
+++ src/test/java/com/github/mrodz/numbertextconverter/NumberServiceEndpointTest.java
@@ -1,11 +1,62 @@
 package com.github.mrodz.numbertextconverter;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumberServiceEndpointTest {
     NumberServiceEndpoint n = new NumberServiceEndpoint();
+
+    @Test
+    public void crash1() {
+        assertThrows(NumberFormatException.class, () -> n.getConversion("1.5"));
+    }
+
+    @Test
+    public void crash2() {
+        assertThrows(NumberFormatException.class, () -> n.getConversion("This is Not a Number"));
+    }
+
+    @Test
+    public void allZeroes() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("000000000");
+        assertEquals("zero", conversion.getResult());
+    }
+
+    @Test
+    public void leadingZeroes() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("00000000005550");
+        assertEquals("five thousand five hundred fifty", conversion.getResult());
+    }
+
+    @Test
+    public void negativeWithLeadingZeroes() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("-0000000000123");
+        assertEquals("negative one hundred twenty three", conversion.getResult());
+    }
+
+    @Test
+    public void negativeSevenHundredSeventySeven() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("-777");
+        assertEquals("negative seven hundred seventy seven", conversion.getResult());
+    }
+
+    @Test
+    public void negativeTenTrillion() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("-10000000000000");
+        assertEquals("negative ten trillion", conversion.getResult());
+    }
+
+    @Test
+    public void one() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("1");
+        assertEquals("one", conversion.getResult());
+    }
+
+    @Test
+    public void zero() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("0");
+        assertEquals("zero", conversion.getResult());
+    }
 
     @Test
     public void five() {
@@ -20,7 +71,19 @@ class NumberServiceEndpointTest {
     }
 
     @Test
-    public void tenThousand() {
+    public void twenty() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("20");
+        assertEquals("twenty", conversion.getResult());
+    }
+
+    @Test
+    public void twentyOne() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("21");
+        assertEquals("twenty one", conversion.getResult());
+    }
+
+    @Test
+    public void ten() {
         NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("10");
         assertEquals("ten", conversion.getResult());
     }
@@ -29,5 +92,113 @@ class NumberServiceEndpointTest {
     public void thirtyThree() {
         NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("33");
         assertEquals("thirty three", conversion.getResult());
+    }
+
+    @Test
+    public void fourHundred() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("400");
+        assertEquals("four hundred", conversion.getResult());
+    }
+
+    @Test
+    public void fourHundredTwenty() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("420");
+        assertEquals("four hundred twenty", conversion.getResult());
+    }
+
+    @Test
+    public void oneThousand() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("1000");
+        assertEquals("one thousand", conversion.getResult());
+    }
+
+    @Test
+    public void twoThousand() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("2000");
+        assertEquals("two thousand", conversion.getResult());
+    }
+
+    @Test
+    public void threeThousand() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("3000");
+        assertEquals("three thousand", conversion.getResult());
+    }
+
+    @Test
+    public void fourThousand() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("4000");
+        assertEquals("four thousand", conversion.getResult());
+    }
+
+    @Test
+    public void fiveThousand() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("5000");
+        assertEquals("five thousand", conversion.getResult());
+    }
+
+    @Test
+    public void sixThousand() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("6000");
+        assertEquals("six thousand", conversion.getResult());
+    }
+
+    @Test
+    public void sevenThousand() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("7000");
+        assertEquals("seven thousand", conversion.getResult());
+    }
+
+    @Test
+    public void eightThousand() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("8000");
+        assertEquals("eight thousand", conversion.getResult());
+    }
+
+    @Test
+    public void nineThousand() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("9000");
+        assertEquals("nine thousand", conversion.getResult());
+    }
+
+    @Test
+    public void tenThousand() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("10000");
+        assertEquals("ten thousand", conversion.getResult());
+    }
+
+    @Test
+    public void fourHundredFour() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("404");
+        assertEquals("four hundred four", conversion.getResult());
+    }
+
+    @Test
+    public void bigNumber1() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("10000002");
+        assertEquals("ten million two", conversion.getResult());
+    }
+
+    @Test
+    public void bigNumber2() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("69420");
+        assertEquals("sixty nine thousand four hundred twenty", conversion.getResult());
+    }
+
+    @Test
+    public void bigNumber3() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("1000000000001");
+        assertEquals("one trillion one", conversion.getResult());
+    }
+
+    @Test
+    public void bigNumber4() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("123456765415789917208123");
+        assertEquals("one hundred twenty three sextillion four hundred fifty six quintillion seven hundred sixty five quadrillion four hundred fifteen trillion seven hundred eighty nine billion nine hundred seventeen million two hundred eight thousand one hundred twenty three", conversion.getResult());
+    }
+
+    @Test
+    public void bigNumber5() {
+        NumberServiceEndpoint.ConvertedNumber conversion = n.getConversion("9728738668283111");
+        assertEquals("nine quadrillion seven hundred twenty eight trillion seven hundred thirty eight billion six hundred sixty eight million two hundred eighty three thousand one hundred eleven", conversion.getResult());
     }
 }
